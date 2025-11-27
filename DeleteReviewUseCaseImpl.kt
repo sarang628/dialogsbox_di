ab700d7cdf9a746963ae7fe44b4d3bca722d1f57
sryang <sarang628@gmail.com>
2025-11-27 11:20:54 +0900

    update

---
+++ DeleteReviewUseCaseImpl.kt
@@ -0,0 +1,23 @@
+package com.sarang.torang.di.dialogsbox_di
+
+import com.sarang.torang.DeleteReviewUseCase
+import com.sarang.torang.repository.FeedRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DeleteReviewUseCaseImpl {
+    @Provides
+    fun providesDeleteReviewUseCase(
+        feedRepository: FeedRepository
+    ): DeleteReviewUseCase {
+        return object : DeleteReviewUseCase {
+            override suspend fun invoke(reviewId : Int) {
+                feedRepository.deleteById(reviewId)
+            }
+        }
+    }
+}
