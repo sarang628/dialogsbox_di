98b0baafa1dc1c77bd558df17289ae178e7fce68
sryang <sarang628@gmail.com>
2025-12-20 15:03:15 +0900

    update feed module

--- DeleteReviewUseCaseImpl.kt
+++ DeleteReviewUseCaseImpl.kt
@@ -1,7 +1,7 @@
 package com.sarang.torang.di.dialogsbox_di
 
 import com.sarang.torang.dialogsbox.usecase.DeleteReviewUseCase
-import com.sarang.torang.repository.FeedRepository
+import com.sarang.torang.repository.feed.FeedRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
